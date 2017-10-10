56bd9a5f217f24cbbdef48fdb93f8a8db3e83538
Milton Sosa <milton.sosa.22@gmail.com>
2017-10-10 19:50:55 -0300

    about page almost finished

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { PopoverController } from 'ionic-angular';
-
-import { PopoverPage } from '../about-popover/about-popover';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  conferenceDate = '2047-05-17';
+	pushPage: any;
 
-  constructor(public popoverCtrl: PopoverController) { }
-
-  presentPopover(event: Event) {
-    let popover = this.popoverCtrl.create(PopoverPage);
-    popover.present({ ev: event });
-  }
-
-  openPage(page: somePage){
- 	this.navCtrl.push(somePage);
-  }
+  constructor(public navCtrl: NavController) {}
 }
